8e94f9b5ecc1b46c42ef9890bc80b1c85a1b73f0
Amine TABOU <amin.tabou@gmail.com>
2023-08-17 13:47:19 +0200

    adding animation on labels

--- src/components/content/dash/sub-dash/PieIndicator.jsx
+++ src/components/content/dash/sub-dash/PieIndicator.jsx
@@ -1,9 +1,18 @@
 import ChartLabel from 'components/shared/ChartLabel';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import styled from 'styled-components';
 const StyledRow = styled(Row)`
   &.pie-indicator-row {
+    &.fade-in {
+      opacity: 0.2;
+    }
+    &.fade-in-demi {
+      opacity: 0.6;
+    }
+    &.fade-out {
+      opacity: 1;
+    }
     & > .col {
       &:nth-child(1) {
         flex-basis: 50px;
@@ -18,10 +27,20 @@ const StyledRow = styled(Row)`
     }
   }
 `;
+const FADE_INTERVAL_MS = 2000;
 const PieIndicator = ({item, color}) => {
   const {name, value} = item;
+  const [fadeProp, setFadeProp] = useState('fade-in');
+  useEffect(() => {
+    setTimeout(() => {
+      setFadeProp('fade-in-demi');
+    }, FADE_INTERVAL_MS / 2);
+    setTimeout(() => {
+      setFadeProp('fade-out');
+    }, FADE_INTERVAL_MS);
+  }, [])
   return (
-    <StyledRow className="pie-indicator-row">
+    <StyledRow className={`pie-indicator-row ${fadeProp}`}>
       <Col className="pie-indicator-1">
         <ChartLabel color={color} />
       </Col>
